9f163efeb2a6538ac2f89fe1ce834b03c7af7983
sitogi <takano162@gmail.com>
2019-07-14 09:50:56 +0900

    tmux の設定ファイルを追加

--- createLink.sh
+++ createLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
