badf356798b7d288bbdbaf592ae1a4c3d7136181
sitogi <takano162@gmail.com>
2019-07-27 15:35:34 +0900

    .ideavimrc を追加
    内容は .vimrc のコピー

--- createLink.bat
+++ createLink.bat
@@ -1,4 +1,5 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
 mklink /D %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 exit 0
 
--- createLink.sh
+++ createLink.sh
@@ -3,5 +3,6 @@ ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
