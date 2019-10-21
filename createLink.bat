121c796d6d62a8b8fb299bbf242dd1aba7f72870
sitogi <takano162@gmail.com>
2019-10-21 23:50:19 +0900

    add VSCode setting files.

--- createLink.bat
+++ createLink.bat
@@ -1,5 +1,7 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
 mklink /D %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+mklink %HOMEPATH%"\AppData\Roaming\Code\User\settings.json" %HOMEPATH%"\dotfiles\.vscode\settings.json"
+mklink %HOMEPATH%"\AppData\Roaming\Code\User\keybindings.json" %HOMEPATH%"\dotfiles\.vscode\keybindings.json"
 exit 0
 
--- createLink.sh
+++ createLink.sh
@@ -10,3 +10,7 @@ mkdir -p ~/.vim/rc
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 
+mkdir -p ~/.config/Code/User
+ln -sf ~/dotfiles/.vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/dotfiles/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
+
