b77c18d564c65337c0a84745d2b940f524624621
sitogi <takano162@gmail.com>
2020-01-12 09:30:53 +0900

    auto installation of VSCode extensions

---
+++ .vscode/installExtensions.sh
@@ -0,0 +1,12 @@
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension mhutchie.git-graph
+code --install-extension oderwat.indent-rainbow
+code --install-extension k--kato.intellij-idea-keybindings
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension esbenp.prettier-vscode
+code --install-extension hex-ci.stylelint-plus
+code --install-extension vscodevim.vim
+
--- createLink.sh
+++ createLink.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# dotfiles
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
@@ -6,13 +8,14 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
+# Vim plugin Settings
 mkdir -p ~/.vim/rc
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 
+# VSCode Settings
 mkdir -p ~/.config/Code/User
 ln -sf ~/dotfiles/.vscode/settings.json ~/.config/Code/User/settings.json
 ln -sf ~/dotfiles/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
-
-echo "Deploy .vscode/extensions.json to your project root dir";
+bash ./.vscode/installExtensions.sh
 
