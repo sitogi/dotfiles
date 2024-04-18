52b941bfc8f593ff19ab576ec249f21cde23483a
sitogi <takano162@gmail.com>
2024-04-18 23:13:58 +0900

    wezterm: support custom link rules out of git

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,13 +13,16 @@ end
 -- 追加したもの
 config.font = wezterm.font("Moralerspace Neon", {weight="Regular", stretch="Normal", style="Normal"})
 config.font_size = 16.5
-
 config.color_scheme = 'Orangish (terminal.sexy)'
-
 config.window_background_opacity = 0.75
-
 config.hide_tab_bar_if_only_one_tab = true
 
+-- カスタムリンクルール
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+local status, customLinkRules = pcall(require, "custom_link_rules")
+if status then
+  table.insert(config.hyperlink_rules, customLinkRules)
+end
 
 -- and finally, return the configuration to wezterm
 return config
