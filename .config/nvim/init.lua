c4761503596f3b5626b746ea79ad4875ff618c3a
sitogi <takano162@gmail.com>
2025-12-10 22:32:57 +0900

    remove not working vim key mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -461,7 +461,6 @@ require("lazy").setup({
           -- キーマッピング
           local opts = { buffer = bufnr, noremap = true, silent = true }
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
           vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', '<leader>R', vim.lsp.buf.rename, opts)
